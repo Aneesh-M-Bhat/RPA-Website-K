e3d5b62fe9ab39a314a6a98265b3b62056af5ba5
Aneesh-M-Bhat <aneeshbhat3719@gmail.com>
2022-06-19 20:39:19 +0530

    5th req done

--- frontend/src/components/Profile/LeaveRequestTable.js
+++ frontend/src/components/Profile/LeaveRequestTable.js
@@ -1,5 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 
 export default function LeaveRequestTable(props) {
-  return <Table></Table>;
+  const [lRDetails, setLRDetails] = useState([]);
+  useEffect(() => {
+    getLeaveRequest();
+  }, []);
+  const getLeaveRequest = async () => {
+    const response = await axios.get(`http://localhost:5000/leaveRequest/get`);
+    setLRDetails(response.data);
+  };
+  return (
+    <Table bordered>
+      <thead>
+        <tr>
+          <th>NO.</th>
+          <th>EMPLOYEE ID</th>
+          <th>EMPLOYEE NAME</th>
+          <th>FROM</th>
+          <th>TO</th>
+          <th>LEAVE TYPE</th>
+          <th>FULL/HALF DAY</th>
+          <th>STATUS</th>
+        </tr>
+      </thead>
+      <tbody>
+        {lRDetails.map((item, index) => {
+          // console.log(item);
+          return (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{item.employeeId}</td>
+              <td>{item.employeeName}</td>
+              <td>{item.from}</td>
+              <td>{item.to}</td>
+              <td>{item.leaveType}</td>
+              <td>{item.duration}</td>
+              <td>{item.status}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
 }
